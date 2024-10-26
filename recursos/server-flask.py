a9e0a0f0d45baaceeb408427df09022a931eb986
GabriellyVitoria5 <assuncaogabrielly5@gmail.com>
2024-10-26 17:02:29 -0300

    validando e alterando métodos http

--- recursos/server-flask.py
+++ recursos/server-flask.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # consultar registros pelo nome
 @app.route('/', methods=['GET'])
-def query_records():
+def get_records():
     name = request.args.get('name')
     
     with open('data.txt', 'r') as f:
@@ -19,7 +19,7 @@ def query_records():
             if record['name'] == str(name):
                 return jsonify(record)
         
-        return jsonify({'error': 'data not found'})
+        return jsonify({'error': 'data not found'}), 404
 
 # listar todos os registros
 @app.route('/all', methods=['GET'])
@@ -27,6 +27,10 @@ def get_all_records():
     with open('data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
+
+        if not records:
+            return jsonify({"error": "any data found."}), 404
+        
         return jsonify(records)
 
 # criar novos registros
@@ -42,17 +46,21 @@ def create_record():
     else:
         records = json.loads(data)
         records.append(record)
+
+    if record not in records:
+        return jsonify({"error": "falied to add new record."}), 500
     
     with open('data.txt', 'w') as f:
         f.write(json.dumps(records, indent=2))
     
-    return jsonify(record)
+    return jsonify({"record added": record}), 201
 
 # atualizar todo o registro com base no nome (precisa de todas os campos do registro para alterar)
 @app.route('/', methods=['PUT'])
-def update_record():
+def update_entire_record():
     record = json.loads(request.data)
     new_records = []
+    updated = False
     
     with open('data.txt', 'r') as f:
         data = f.read()
@@ -60,39 +68,50 @@ def update_record():
     
     for r in records:
         if r['name'] == record['name']:
-            r['email'] = record['email']
+            r = record # substitui o registro antigo pelo novo
+            updated = True
         new_records.append(r)
+
+    if not updated:
+        return jsonify({"error": "any record with this name was found."}), 404
     
     with open('data.txt', 'w') as f:
         f.write(json.dumps(new_records, indent=2))
     
     return jsonify(record)
 
-# apagar um  registro pelo nome (reescreve o arquivo sem o registro com o nome inserido)    
+# apagar um  registro pelo nome (reescreve o arquivo sem o registro com o nome inserido na requisição)    
 @app.route('/', methods=['DELETE'])
 def delte_record():
     record = json.loads(request.data)
     new_records = []
+    deleted = False
     
     with open('data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
+        
         for r in records:
             if r['name'] == record['name']:
+                deleted = True
                 continue
             new_records.append(r)
+
+    if not deleted:
+        return jsonify({"error": "any record with this name was found."}), 404
     
     with open('data.txt', 'w') as f:
         f.write(json.dumps(new_records, indent=2))
     
-    return jsonify(record)
+    return jsonify({"deleted_record": record})
 
 # atualizar parcialmente o registro pelo nome
 @app.route('/', methods=['PATCH'])
-def patch_user():
+def partial_update_record():
     name = request.args.get('name')
     record = json.loads(request.data)
     new_records = []
+    updated = False
     
     with open('data.txt', 'r') as f:
         data = f.read()
@@ -104,16 +123,19 @@ def patch_user():
                 r["name"] = record["name"]
             if "email" in data:
                 r["email"] = record["email"]
+            updated = True
         new_records.append(r)
 
+    if not updated:
+        return jsonify({"error": "any record with this name was found."}), 404
 
     with open('data.txt', 'w') as f:
         f.write(json.dumps(new_records, indent=2))
 
-    return jsonify(new_records)
+    return jsonify({"updated record": new_records})
 
 # mostrar opções de requisição permitidas
-@app.route('/*', methods=['OPTIONS'])
+@app.route('/', methods=['OPTIONS'])
 def options_record():
     response = make_response()
     response.headers['Allow'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
